833c4723be668fc534c09462969c0cd90862b40b
Máté Boér <mate.boer@risingstack.com>
2018-10-10 11:15:08 +0200

    fix app

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, '../client', 'build')));
 
 
 export default app;
